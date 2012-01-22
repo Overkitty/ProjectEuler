1ec9cd416ec51b4e110c2d61ddb121b281ff15d0
Keaton VanAuken <overkitty@gmail.com>
2012-01-23 00:43:15 +0100

    Completed Problem 2!

--- Problem2/Program.cs
+++ Problem2/Program.cs
@@ -1,14 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 
 namespace Problem2
 {
     class Program
     {
+        private const long Max = 4000000;
+        private const int Reps = 1000000;
+
+        private long _total;
+        private int _step;
+
         static void Main(string[] args)
         {
+            var program = new Program();
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            for (int i = 0; i < Reps; i++ )
+            {
+                // program.DoProblem();
+                program.DoProblem2();
+            }
+            stopWatch.Stop();
+
+            Console.WriteLine(string.Format("DoProblem: {0} ms", stopWatch.ElapsedMilliseconds));
+            Console.ReadLine();
+        }
+
+        private void DoProblem()
+        {
+            long small = 1;
+            long big = 2;
+
+            _total = 0;
+            while (big < Max)
+            {
+                if(big % 2 == 0)
+                {
+                    _total += big;
+                }
+
+                long temp = small + big;
+                small = big;
+                big = temp;
+            }
+
+            // Console.WriteLine(string.Format("Total: {0}", _total));
+        }
+
+        private void DoProblem2()
+        {
+            long small = 1;
+            long big = 2;
+
+            _total = 0;
+            _step = 3;
+            while (big < Max)
+            {
+                if (_step == 3)
+                {
+                    _total += big;
+                    _step = 0;
+                }
+
+                _step++;
+
+                long temp = small + big;
+                small = big;
+                big = temp;
+            }
+
+            // Console.WriteLine(string.Format("Total2: {0}", _total));
         }
     }
 }
---
+++ Problem3/Program.cs
@@ -0,0 +1,12 @@
+﻿/* The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143? */
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
